658d27120b622278b1c3e7126a9e7281dfeb60c6
Thibault Zanini <thibaultzanini@gmail.com>
2024-11-14 15:49:58 +0100

    Added Base FormField component
    Base component for form fields

---
+++ ui/form-fields/form-field.mod/form-field.js
@@ -0,0 +1,42 @@
+const { Component } = require("mod/ui/component");
+
+/**
+ * @class FormField
+ * @extends Component
+ * @description Base component for form fields
+ */
+exports.FormField = class FormField extends Component {
+    /** @protected */
+    _label = null;
+
+    /**
+     * @description Indicates whether the form field is enabled
+     * @public
+     * @type {boolean}
+     */
+    isEnabled = true;
+
+    /**
+     * @description Indicates whether the form field is mandatory
+     * @public
+     * @type {boolean}
+     */
+    isRequired = false;
+
+    /**
+     * @description The label of the form field
+     * @public
+     * @type {string}
+     */
+    get label() {
+        let displayedLabel = this._label ?? "Field";
+
+        if (this.isRequired) displayedLabel += " *";
+
+        return displayedLabel;
+    }
+
+    set label(value) {
+        this._label = value;
+    }
+};
